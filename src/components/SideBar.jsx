38e9af217bc8d83832cfa309146a6d21161557a1
JordanClough <jordan_clough@sfu.ca>
2024-12-28 15:02:59 -0800

    base changes

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,19 +1,20 @@
 import { NavLink } from 'react-router-dom';
 
-function SideBar(){
-    return(
+function SideBar() {
+    return (
         <header className="sidebar">
             <h1>Jordan Clough</h1>
             <h2>Software Developer</h2>
             <nav>
                 <ul>
-                    <li><NavLink to="/" exact className={({isActive}) => isActive ? 'active' : ''}>Home</NavLink></li>
-                    <li><NavLink to="/projects" className={({isActive}) => isActive ? 'active' : ''}>Projects</NavLink></li>
-                    <li><NavLink to="/skills" className={({isActive}) => isActive ? 'active' : ''}>Skills</NavLink></li>
-                    <li><NavLink to="/contact" className={({isActive}) => isActive ? 'active' : ''}>Contact</NavLink></li>
+                    <li><NavLink to="/" end className={({ isActive }) => isActive ? 'active' : ''}>Home</NavLink></li>
+                    <li><NavLink to="/projects" className={({ isActive }) => isActive ? 'active' : ''}>Projects</NavLink></li>
+                    <li><NavLink to="/skills" className={({ isActive }) => isActive ? 'active' : ''}>Skills</NavLink></li>
+                    <li><NavLink to="/contact" className={({ isActive }) => isActive ? 'active' : ''}>Contact</NavLink></li>
                 </ul>
             </nav>
         </header>
     );
 }
-export default SideBar
+
+export default SideBar;
